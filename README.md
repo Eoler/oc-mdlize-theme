39eb237239e252e15a15f94b2e4d96bf028b78da
Danilo Cuculic <danilo.cuculic@gmail.com>
2016-07-12 13:54:33 +0200

    1.1.0: Added Gulp-based SASS/ES6 build, removed obsolete font assets

--- README.md
+++ README.md
@@ -16,3 +16,25 @@ Where `<name>` is whatever you specified in `theme:install`.
 
 ## Customize
 If you are not planning to use Materialize icons, remove this line: `<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">`
+
+Comment out unwanted component styles / scripts to create custom Materialize library in:
+- assets/scss/materialize.scss
+- assets/es6/materialize.js
+
+Development changes can be automated with command-line instruction inside *theme root*:
+```
+gulp watch
+```
+Build optimized, minified, autoprefixed assets with command-line instruction inside *theme root*:
+```
+gulp --production
+```
+
+## One-time Development Environment Setup
+Install [NodeJS and Node Package Manager](https://nodejs.org/en/) globally (Stable version preferred).
+Run the following command-line instructions inside *theme root*:
+```
+npm install --global gulp bower
+npm install && bower install
+```
+
---
+++ assets/es6/materialize.js
@@ -0,0 +1,37 @@
+
+//= require ../vendor/materialize/js/initial.js
+//= require ../vendor/materialize/js/jquery.easing.1.3.js
+//= require ../vendor/materialize/js/animation.js
+//= require ../vendor/materialize/js/velocity.min.js
+//= require ../vendor/materialize/js/hammer.min.js
+//= require ../vendor/materialize/js/jquery.hammer.js
+//= require ../vendor/materialize/js/global.js
+//= require ../vendor/materialize/js/collapsible.js
+//= require ../vendor/materialize/js/dropdown.js
+//= require ../vendor/materialize/js/leanModal.js
+//= require ../vendor/materialize/js/materialbox.js
+//= require ../vendor/materialize/js/parallax.js
+//= require ../vendor/materialize/js/tabs.js
+//= require ../vendor/materialize/js/tooltip.js
+//= require ../vendor/materialize/js/waves.js
+//= require ../vendor/materialize/js/toasts.js
+//= require ../vendor/materialize/js/sideNav.js
+//= require ../vendor/materialize/js/scrollspy.js
+//= require ../vendor/materialize/js/forms.js
+//= require ../vendor/materialize/js/slider.js
+//= require ../vendor/materialize/js/cards.js
+//= require ../vendor/materialize/js/chips.js
+//= require ../vendor/materialize/js/pushpin.js
+//= require ../vendor/materialize/js/buttons.js
+//= require ../vendor/materialize/js/transitions.js
+//= require ../vendor/materialize/js/scrollFire.js
+//= require ../vendor/materialize/js/date_picker/picker.js
+//= require ../vendor/materialize/js/date_picker/picker.date.js
+//= require ../vendor/materialize/js/character_counter.js
+//= require ../vendor/materialize/js/carousel.js
+
+jQuery(document).ready(function($){
+
+    // add default initialization here
+
+});
